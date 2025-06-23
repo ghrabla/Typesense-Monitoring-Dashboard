d33ebf852c03fbd600c4a900cddb868a8f4eea2f
ghrabla <kamalghrabla@gmail.com>
2025-06-23 16:08:11 +0100

    init commit

---
+++ index.go
@@ -0,0 +1,39 @@
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+var (
+	client = &http.Client{                                                                                                                                                                                                                                                                                                   
+		Timeout: 10 * time.Second,
+	}
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "OK")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Server starting on port %s...\n", port)
+	if err := http.ListenAndServe(":"+port, r); err != nil {
+		log.Fatal(err)
+	}
+}
